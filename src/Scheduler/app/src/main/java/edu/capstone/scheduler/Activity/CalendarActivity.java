e74e734c646723cb332fd96f2a9430c33d4adfd1
jonghyeon <jinos01@naver.com>
2020-10-12 11:23:06 +0900

    토요일 일요일 색상 추가

--- src/Scheduler/app/src/main/java/edu/capstone/scheduler/Activity/CalendarActivity.java
+++ src/Scheduler/app/src/main/java/edu/capstone/scheduler/Activity/CalendarActivity.java
@@ -1,12 +1,15 @@
 package edu.capstone.scheduler.Activity;
 
 import android.os.Bundle;
+import android.view.Window;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.CalendarMode;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
 
 import java.util.Calendar;
 
@@ -18,13 +21,29 @@ public class CalendarActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_calendar);
 
         materialCalendarView = (MaterialCalendarView) findViewById(R.id.calenarView);
 
+        //달력 시작과 끝 지정
         materialCalendarView.state().edit()
                 .setFirstDayOfWeek(Calendar.SUNDAY)
                 .setCalendarDisplayMode(CalendarMode.MONTHS)
                 .commit();
+
+        //일요일 토요일 색 변경
+        materialCalendarView.addDecorators(new SundayDecorator(), new SaturdayDecorator());
+
+        //날짜 클릭시 일정 출력
+        materialCalendarView.setOnDateChangedListener(new OnDateSelectedListener(){
+            @Override
+            public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected){
+                setContentView(R.layout.activity_list);
+
+            }
+        });
+
     }
+
 }
