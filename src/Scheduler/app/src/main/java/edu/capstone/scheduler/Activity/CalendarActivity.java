3158362102c9fb7a98d987cda91cf65e4158e561
jonghyeon <jinos01@naver.com>
2020-10-23 16:25:15 +0900

    버튼 추가

--- src/Scheduler/app/src/main/java/edu/capstone/scheduler/Activity/CalendarActivity.java
+++ src/Scheduler/app/src/main/java/edu/capstone/scheduler/Activity/CalendarActivity.java
@@ -1,11 +1,15 @@
 package edu.capstone.scheduler.Activity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.CalendarMode;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
@@ -18,6 +22,7 @@ import edu.capstone.scheduler.R;
 public class CalendarActivity extends AppCompatActivity {
 
     MaterialCalendarView materialCalendarView;
+    private long lastTimeBackPressed;
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -44,6 +49,55 @@ public class CalendarActivity extends AppCompatActivity {
             }
         });
 
+        // 하단 버튼 클릭 리스너
+        Button todaybtn = findViewById(R.id.todaybtn);
+        Button calendatbtn = findViewById(R.id.calendarbtn);
+        Button idbtn = findViewById(R.id.idbtn);
+
+        todaybtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Toast.makeText(getApplicationContext(),"test",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        calendatbtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Toast.makeText(getApplicationContext(),"test",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        idbtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Toast.makeText(getApplicationContext(),"test",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // 플로팅 버튼
+        FloatingActionButton plusbtn =  findViewById(R.id.plusbtn);
+
+        plusbtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Toast.makeText(getApplicationContext(),"test",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
+    //종료 버튼
+    @Override
+    public void onBackPressed(){
+        if(System.currentTimeMillis() - lastTimeBackPressed < 1500){
+            finish();
+            return;
+        }
+
+        Toast.makeText(this,"한번 더 누르면 종료합니다.", Toast.LENGTH_SHORT).show();
+
+        lastTimeBackPressed = System.currentTimeMillis();
+    }
+
 
 }
