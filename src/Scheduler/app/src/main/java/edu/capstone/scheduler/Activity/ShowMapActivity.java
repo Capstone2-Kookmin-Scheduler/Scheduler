e7eb5d6b202b5b1d59db2d78fa7c759fa6b9eb9a
kooym5607 <starkoo06@kookmin.ac.kr>
2020-11-12 21:37:02 +0900

    합침

--- src/Scheduler/app/src/main/java/edu/capstone/scheduler/Activity/ShowMapActivity.java
+++ src/Scheduler/app/src/main/java/edu/capstone/scheduler/Activity/ShowMapActivity.java
@@ -1,21 +1,24 @@
 package edu.capstone.scheduler.Activity;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.NotificationManager;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.location.Location;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 
 import com.mapbox.android.core.location.LocationEngine;
 import com.mapbox.android.core.location.LocationEngineCallback;
@@ -49,8 +52,22 @@ import com.mapbox.mapboxsdk.style.layers.LineLayer;
 import com.mapbox.mapboxsdk.style.layers.Property;
 import com.mapbox.mapboxsdk.style.layers.SymbolLayer;
 import com.mapbox.mapboxsdk.style.sources.GeoJsonSource;
+import com.odsay.odsayandroidsdk.API;
+import com.odsay.odsayandroidsdk.ODsayData;
+import com.odsay.odsayandroidsdk.ODsayService;
+import com.odsay.odsayandroidsdk.OnResultCallbackListener;
 
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.lang.ref.WeakReference;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.List;
 
 import edu.capstone.scheduler.R;
@@ -81,6 +98,7 @@ public class ShowMapActivity extends BaseActivity implements OnMapReadyCallback,
     private ShowMapActivityLocationCallback callback = new ShowMapActivityLocationCallback(this);
     private LocationEngine locationEngine;
 
+    private TextView information;
     private MapView mapView;
     private MapboxMap mapboxMap;
     private PermissionsManager permissionsManager;
@@ -90,11 +108,13 @@ public class ShowMapActivity extends BaseActivity implements OnMapReadyCallback,
     private MapboxDirections client;
     private static DirectionsRoute currentRoute;
 
+    private Double firstStation_lat, firstStation_lng;
     private int NOTI_ID;
     private Double lat;
     private Double lng;
     private Point origin;
     private Point destination;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -107,14 +127,14 @@ public class ShowMapActivity extends BaseActivity implements OnMapReadyCallback,
             NOTI_ID = extras.getInt("NOTI_ID");
             lat = extras.getDouble("lat");
             lng = extras.getDouble("lng");
-            origin = Point.fromLngLat(lng,lat);
-            destination = Point.fromLngLat(extras.getDouble("arrival_lng"),extras.getDouble("arrival_lat"));
+            origin = Point.fromLngLat(lng, lat);
+            destination = Point.fromLngLat(extras.getDouble("arrival_lng"), extras.getDouble("arrival_lat"));
         }
-
+        find_route_with_odsay(getApplicationContext());
         mapView = (MapView) findViewById(R.id.mapView);
         mapView.onCreate(savedInstanceState);
 
-        Log.e("latlng","는 "+lat+"      "+lng);
+        Log.e("latlng", "는 " + lat + "      " + lng);
         mapView.getMapAsync(this);
 
         NotificationManager nm = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
@@ -130,13 +150,13 @@ public class ShowMapActivity extends BaseActivity implements OnMapReadyCallback,
             public void onStyleLoaded(@NonNull Style style) {
                 // 버스정류장/지하철역 마커뷰 표시
                 markerViewManager = new MarkerViewManager(mapView, ShowMapActivity.this.mapboxMap);
-                View customView = LayoutInflater.from(ShowMapActivity.this).inflate(R.layout.marker_view,null);
-                customView.setLayoutParams(new FrameLayout.LayoutParams(WRAP_CONTENT,WRAP_CONTENT));
+                View customView = LayoutInflater.from(ShowMapActivity.this).inflate(R.layout.marker_view, null);
+                customView.setLayoutParams(new FrameLayout.LayoutParams(WRAP_CONTENT, WRAP_CONTENT));
                 TextView titleTextView = customView.findViewById(R.id.marker_window_title);
                 titleTextView.setText("제목");
                 TextView snippetTextView = customView.findViewById(R.id.marker_window_snippet);
                 snippetTextView.setText("내용");
-                markerView = new MarkerView(new LatLng(destination.latitude(),destination.longitude()),customView);
+                markerView = new MarkerView(new LatLng(destination.latitude(), destination.longitude()), customView);
                 markerViewManager.addMarker(markerView);
 
                 LocalizationPlugin localizationPlugin = new LocalizationPlugin(mapView, ShowMapActivity.this.mapboxMap, style);
@@ -149,7 +169,7 @@ public class ShowMapActivity extends BaseActivity implements OnMapReadyCallback,
 
                 initLayers(style);
                 initSource(style);
-                getRoute(origin,destination);
+
 
 
                 // Map is set up and the style has loaded. Now you can add data or make other map adjustments
@@ -161,7 +181,7 @@ public class ShowMapActivity extends BaseActivity implements OnMapReadyCallback,
     private void initSource(@NonNull Style loadedMapStyle) {
         loadedMapStyle.addSource(new GeoJsonSource(ROUTE_SOURCE_ID));
 
-        GeoJsonSource iconGeoJsonSource = new GeoJsonSource(ICON_SOURCE_ID, FeatureCollection.fromFeatures(new Feature[] {
+        GeoJsonSource iconGeoJsonSource = new GeoJsonSource(ICON_SOURCE_ID, FeatureCollection.fromFeatures(new Feature[]{
                 Feature.fromGeometry(Point.fromLngLat(origin.longitude(), origin.latitude())),
                 Feature.fromGeometry(Point.fromLngLat(destination.longitude(), destination.latitude()))}));
         loadedMapStyle.addSource(iconGeoJsonSource);
@@ -189,13 +209,13 @@ public class ShowMapActivity extends BaseActivity implements OnMapReadyCallback,
                 iconImage(RED_PIN_ICON_ID),
                 iconIgnorePlacement(true),
                 iconAllowOverlap(true),
-                iconOffset(new Float[] {0f, -9f})));
+                iconOffset(new Float[]{0f, -9f})));
     }
 
 
     //Point.fromLngLat()로 파라미터 넣기
     private void getRoute(Point origin, Point destination) { // 지도상 경로 받아오기
-        Log.e("경로의 첫",origin.toString());
+        Log.e("경로의 첫", origin.toString());
         Log.e("경로의 끝", destination.toString());
         client = MapboxDirections.builder()
                 .origin(origin)//출발지 위도 경도
@@ -253,9 +273,20 @@ public class ShowMapActivity extends BaseActivity implements OnMapReadyCallback,
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        if(locationEngine!=null){
+        if (locationEngine != null) {
             locationEngine.removeLocationUpdates(callback);
         }
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mapboxMap.getLocationComponent().setLocationComponentEnabled(false);
         mapView.onDestroy();
     }
 
@@ -337,9 +368,8 @@ public class ShowMapActivity extends BaseActivity implements OnMapReadyCallback,
                 if (location == null) {
                     return;
                 }
-                lat = result.getLastLocation().getLatitude();
-                lng = result.getLastLocation().getLongitude();
-                origin = Point.fromLngLat(lng,lat);
+
+                origin = Point.fromLngLat(result.getLastLocation().getLongitude(),result.getLastLocation().getLatitude());
                 getRoute(origin,destination);
                 // Pass the new location to the Maps SDK's LocationComponent
                 if (activity.mapboxMap != null && result.getLastLocation() != null) {
@@ -370,4 +400,123 @@ public class ShowMapActivity extends BaseActivity implements OnMapReadyCallback,
 
 
     }
+
+    private String find_route() {
+        HttpURLConnection urlConnection = null;
+        StringBuffer stringBuffer = new StringBuffer();
+
+        String address = "https://maps.googleapis.com/maps/api/directions/json?origin=" + lat + "," + lng +
+                "&destination=" + destination.latitude() + "," + destination.longitude() + "&mode=transit&departure_time=now&language=ko&key=AIzaSyCG6NeTZ9cdyvFoz_tNIsBHMJmfCKw1vl0" ;
+        String address1 = "https://maps.googleapis.com/maps/api/directions/json?origin=37.5728359,126.9746922&destination=37.5129907,127.1005382&mode=transit&departure_time=now&language=ko&key=AIzaSyCG6NeTZ9cdyvFoz_tNIsBHMJmfCKw1vl0";
+
+        try {
+            URL url = new URL(address);
+            urlConnection = (HttpURLConnection) url.openConnection();
+
+            urlConnection.setRequestMethod("POST");
+            urlConnection.setRequestProperty("Accept-Charset", "UTF-8");
+            urlConnection.setRequestProperty("Context_Type", "application/x-www-form-urlencoded;cahrset=UTF-8");
+
+            String strParams = stringBuffer.toString();
+            OutputStream os = urlConnection.getOutputStream();
+            os.write(strParams.getBytes("UTF-8"));
+            os.flush(); os.close();
+
+            if (urlConnection.getResponseCode() != HttpURLConnection.HTTP_OK) return null;
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream(), "UTF-8"));
+
+            String line; String page = "";
+
+            while((line = reader.readLine()) != null) {
+                page += line;
+            }
+            return page;
+
+
+        }catch (IOException e) {
+            e.printStackTrace();
+        } // end of catch exception
+
+
+        return null;
+    } // end of find_route method
+
+    private void find_route_with_odsay(Context context){
+        ODsayService odsayService = ODsayService.init(context,"suLGma46yOIqhKYbRFlIXAWLeDWumTQqfmY0RJ+ZnvE");
+        odsayService.setReadTimeout(5000);
+        odsayService.setConnectionTimeout(5000);
+
+        OnResultCallbackListener onResultCallbackListener = new OnResultCallbackListener() {
+            @Override
+            public void onSuccess(ODsayData odsayData, API api) {
+                if(api == API.SEARCH_PUB_TRANS_PATH){
+                    try {
+                        JSONArray jsonArray = odsayData.getJson().getJSONObject("result").getJSONArray("path").getJSONObject(1).getJSONArray("subPath");
+                        String startName = null; String endName = null;
+                        //get firstStation Location with latitude and longitude
+                        for (int i = 0; i<jsonArray.length(); i++) {
+                            int type = jsonArray.getJSONObject(i).getInt("trafficType");
+                            if (type == 3) continue;
+                            else {
+                                firstStation_lat = Double.parseDouble(jsonArray.getJSONObject(i).getString("startY"));
+                                firstStation_lng = Double.parseDouble(jsonArray.getJSONObject(i).getString("startX"));
+                                destination = Point.fromLngLat(firstStation_lng, firstStation_lat);
+                                getRoute(origin, destination);
+                                break;
+                            }
+                        }
+                        //get path with station names
+                        for (int i = 0; i<jsonArray.length(); i++){
+                            int type = jsonArray.getJSONObject(i).getInt("trafficType");
+                            if (type == 3) continue;
+                            else {
+                                startName = jsonArray.getJSONObject(i).getString("startName");
+                                endName = jsonArray.getJSONObject(i).getString("endName");
+                            }
+                            Log.i("출발 목적지", startName+endName);
+                        }
+
+//                        int time = jsonArray.getJSONObject(1).getJSONObject("info").getInt("totalTime");
+//                        Log.i("예상 소요시간 ", Integer.toString(time));
+
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+
+            }
+
+            @Override
+            public void onError(int i, String s, API api) {
+                if(api==API.BUS_STATION_INFO){}
+            }
+        };
+
+        // 출경도,출위도,도경도,도위도,정렬방식(0:추천,1:타입별),검색방식(0:도시내검색,1:도시간검색),경로수단(0:모두,1:지하철,2:버스)
+        odsayService.requestSearchPubTransPath(lng.toString(),lat.toString(),Double.toString(destination.longitude()),Double.toString(destination.latitude()), "0", "0", "0", onResultCallbackListener);
+    }
+
+
+
+
+
+    class NetworkTask extends AsyncTask<Void, Void, String> {
+        @Override
+        protected String doInBackground(Void... voids) {
+            String result;
+            result = find_route();
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            information.setText(s);
+        }
+    }
+
+
 }
